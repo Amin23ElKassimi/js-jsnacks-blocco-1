bad903cccb81db70676cbdcf624e24aa46edef13
Amin23ElKassimi <aminelkassimi01@gmail.com>
2023-11-10 12:05:39 +0100

    Snack01

---
+++ snack01/script.js
@@ -0,0 +1,26 @@
+
+// Dichiarazione Variabili
+
+
+const anniPadre = parseInt (prompt('Quanti anni ha tuo padre'));
+const anniMadre = parseInt (prompt('Quanti anni ha tua madre'));
+
+
+if (anniPadre > anniMadre){ 
+    // Scrivo che il Papa e' piu' grande della mamma
+    console.log('il Papa e piu grande della mamma')
+}
+
+
+else if (anniPadre === anniMadre){ 
+    // Scrivo che il Papa e' piu' grande della mamma
+    console.log('I genitori hanno la stessa eta')
+}
+
+else if (anniPadre < anniMadre){
+        // Scrivo che il Papa e' piu' grande della mamma
+        console.log('La mamma e piu grande della Papa')
+}
+
+
+
